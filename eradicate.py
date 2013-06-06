d6e9f48987332917b5dc57f885dee7a831929f47
rtrjl <rt@zone42.fr>
2013-06-06 18:58:34 +0200

    maj regex

--- eradicate.py
+++ eradicate.py
@@ -17,15 +17,9 @@ for filename in filenames:
     if fileExtension not in fileExtensions:
         continue
 
-    rawstr1 = r"""^\<\?$
-^#d47c75#$
-^echo \"\<script type=\\\"text\/javascript\\\" src=\\\"http:\/\/jstoredirect.net\/qw\/mod.php\?mod=18\\\" name=\\\"qe18\\\"></script>\";$
-^#/d47c75#$
-^\?\>$"""
-    rawstr2 = r"""\<!--d47c75--\>\<script type=\"text/javascript\" src=\"http://jstoredirect.net/qw/mod.php\?mod=18\" name=\"qe18\"\>\<\/script\>\<\!--\/d47c75--\>"""
-    rawstr3 = r"""^\/\*d47c75\*\/$
-^.*$
-^\/\*\/d47c75\*/$"""
+    rawstr1 = r"""^\<\?\r\n^#d47c75.*#\/d47c75#\r\n^\?\>"""
+    rawstr2 = r"""\<!--d47c75--\>.*<\!--\/d47c75--\>"""
+    rawstr3 = r"""^\/\*d47c75\*\/\r\n^.*\r\n\/\*\/d47c75\*\/"""
 
     stringlist=[rawstr1,rawstr2,rawstr3]
 
@@ -33,7 +27,7 @@ for filename in filenames:
         myFileStr = open(filename,'r').read()
 
         # method 1: using a compile object
-        compile_obj = re.compile(rawstr,  re.MULTILINE)
+        compile_obj = re.compile(rawstr,re.MULTILINE|re.DOTALL)
         match = compile_obj.search(myFileStr)
         # Replace string
         if match:
